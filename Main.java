fe4d93696930befaa23d1ab948ec3e0d60a4d505
Jørgen <jorgenba@hiof.no>
2023-05-21 23:01:10 +0200

    push

--- Main.java
+++ Main.java
@@ -1,12 +1,10 @@
 package HIOF.GameEnigne2D;
 
-import HIOF.GameEnigne2D.howto.ExampleWindow;
 import HIOF.GameEnigne2D.modules.window.Window;
 
 public class Main {
     public static void main(String[] args) {
-        //Window window = Game.create(1920, 1080, "Test", 60.0);
-        Window window = ExampleWindow.create(1920, 1080, "Example", 60.0);
+        Window window = Game.create(1920, 1080, "Test", 60.0);
 
         window.run();
     }
